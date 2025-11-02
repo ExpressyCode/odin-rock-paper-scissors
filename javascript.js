de09ea6ae162d7dfb215c7bac2a514748cc05875
Tobias Kanz <19491730+ExpressyCode@users.noreply.github.com>
2025-11-02 13:15:56 +0100

    Fix missing equal sign

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     // generate a random number between 0 and 2
     let randomNumber = Math.floor(Math.random() * 3);
     // return "rock", "paper" or "scissors" based on the random number
-    if (randomNumber == 0) {
+    if (randomNumber === 0) {
         return "rock";
     } else if (randomNumber === 1) {
         return "paper";
