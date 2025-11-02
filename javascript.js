0b0722d0d1f8010dce98c716458d2fd06bab8888
Tobias Kanz <19491730+ExpressyCode@users.noreply.github.com>
2025-11-02 13:12:37 +0100

    Add return of words based on the random number

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 function getComputerChoice() {
     // generate a random number between 0 and 2
     let randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber;
+    // return "rock", "paper" or "scissors" based on the random number
+    if (randomNumber == 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else if (randomNumber === 2) {
+        return "scissors";
+    }
 }
 
 console.log(getComputerChoice())
