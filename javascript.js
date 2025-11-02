66e71962a21f5db77df0bd27b82b3360972ba1d5
Tobias Kanz <19491730+ExpressyCode@users.noreply.github.com>
2025-11-02 13:02:11 +0100

    Created a function getComputerChoice and it can generate a random number between 0 and 2.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    // generate a random number between 0 and 2
+    let randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
+}
+
+console.log(getComputerChoice())
